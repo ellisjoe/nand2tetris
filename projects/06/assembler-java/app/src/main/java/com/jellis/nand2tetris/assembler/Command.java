27635b46caa7708c53df5eb9b9a840c1d9ca7b13
Joe Ellis <ellis125@gmail.com>
2024-02-04 17:10:25 -0700

    Add documentation to Command

--- projects/06/assembler-java/app/src/main/java/com/jellis/nand2tetris/assembler/Command.java
+++ projects/06/assembler-java/app/src/main/java/com/jellis/nand2tetris/assembler/Command.java
@@ -9,7 +9,15 @@ sealed interface Command permits ACommand, LCommand, CCommand {
         };
     }
 
+    /**
+     * Translate any ACommands that have non-numeric symbols into their numeric symbols defined by the given table.
+     * @param table
+     * @return
+     */
     Command translate(SymbolTable table);
 
+    /**
+     * Produce the hack binary instruction for this command.
+     */
     String assemble();
 }
